4f5618c9a52b372684af1511a6261e88da3ebcf5
Clay <radiantclay@gmail.com>
2025-03-21 15:10:37 -0400

    remove ref for linting

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Jersey_10 } from 'next/font/google'
-import Header from '@/components/Header'
 import { Roboto } from 'next/font/google'
-import Footer from '@/components/Footer'
 import { Jaro } from 'next/font/google'
 
 const geistSans = Geist({
