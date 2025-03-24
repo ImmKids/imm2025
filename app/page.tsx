d02feb5308f703a3c847f00a58cdbbf3f5522c45
PixClay <radiantclay@gmail.com>
2025-03-24 08:36:07 -0400

    fix layout

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,10 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-imm-about">
       <MenuWidget />
-      <div className="flex flex-col w-[800px] py-20 items-end justify-end text-[#414759] text-2xl font-bold font-roboto">
-        Grad Show Coming Soon
+      <div className="flex flex-col w-full max-w-[800px] py-20 items-center sm:items-end justify-center sm:justify-end text-[#414759] text-2xl font-bold font-roboto px-4 sm:px-0">
+        <div className="w-full text-center sm:text-right">
+          Grad Show Coming Soon
+        </div>
       </div>
     </div>
   );
